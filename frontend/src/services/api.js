18636635a55d1e1787610421e30688c1c4c0f6c2
Danil Bazylevych <dbazylevych-6171@git.green-lms.app>
2025-12-06 02:34:16 +0200

    fix url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import { BASE_URL } from "../config";
 
-const API_URL = `${BASE_URL}/api`;
+// удаляем слэш на конце
+const API_URL = `${BASE_URL.replace(/\/$/, "")}/api`;
 
 export const register = async (data) => {
   const res = await fetch(`${API_URL}/auth/register`, {
